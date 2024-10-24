6b03edab0970a1aa0c114c15c2f368855064aa75
Tor Idland <tor.idland@nav.no>
2024-10-24 13:06:44 +0200

    :construction_worker: Lagt til Dockerfile, skrudd på standalone build og satt asset_prefix til cdn
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>
    Co-authored-by: Fredrik Meyer <fredrik.meyer@nav.no>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  assetPrefix: process.env.ASSET_PREFIX ?? undefined,
 };
 
 export default nextConfig;
