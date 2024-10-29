939a82277c5fedc40d2270583ea65f46f94492a5
Fredrik Meyer <fredrik.meyer@nav.no>
2024-10-29 17:13:04 +0100

    Bruk nye endepunkter fra statistikk

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,38 @@
-import { hentBehandlingsTidPerDag } from 'lib/services/statistikkService';
+import {
+  hentAntallÅoneBehandlinger,
+  hentAntallÅpneBehandlingerPerAvklaringsbehov,
+  hentBehandlingsTidPerDag,
+} from 'lib/services/statistikkService';
 import styles from './page.module.css';
 import { MyPlot } from './Plot';
 
 export default async function Home() {
   const res = await hentBehandlingsTidPerDag(null);
+  const åpneBehandlinger = await hentAntallÅoneBehandlinger();
+  const perAvklaringsbehov = await hentAntallÅpneBehandlingerPerAvklaringsbehov();
+
   console.log(res);
   return (
     <div className={styles.page}>
       <h1>Produksjonsstyring</h1>
-      <div style={{ width: '90vw', height: '90vh' }}>
-        <MyPlot x={res.map((t) => new Date(t.dag))} y={res.map((t) => t.snitt / 3600)} />
+      <div>
+        <div style={{ display: 'flex', flexDirection: 'column', border: '1px solid black', borderRadius: '5px' }}>
+          <div style={{ textAlign: 'center' }}>
+            <h2>Åpne behandlinger nå</h2>
+          </div>
+          <div style={{ textAlign: 'center' }}>{åpneBehandlinger}</div>
+        </div>
+        <div>
+          <h2> Åpne behandlinger per avklaringsbehov</h2>
+          <ul>
+            {perAvklaringsbehov.map((behov) => (
+              <li key={behov.behov}>{behov.antall}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <MyPlot x={res.map((t) => new Date(t.dag))} y={res.map((t) => t.snitt / 3600)} />
+        </div>
       </div>
     </div>
   );
--- lib/services/statistikkService.ts
+++ lib/services/statistikkService.ts
@@ -8,7 +8,22 @@ interface BehandlingtidPerDag {
   snitt: number;
 }
 
+interface BehandlingPerAvklaringsbehov {
+  antall: number;
+  behov: string;
+}
+
 export const hentBehandlingsTidPerDag = async (behandlingType: string | null) => {
   const url = `${statistikkApiBaseURL}/behandlingstid/${behandlingType || '{typeBehandling}'}`;
   return await fetchProxy<BehandlingtidPerDag[]>(url, statistikkApiScope, 'GET');
+};
+
+export const hentAntallÅoneBehandlinger = async () => {
+  const url = `${statistikkApiBaseURL}/åpne-behandlinger`;
+  return await fetchProxy<number>(url, statistikkApiScope, 'GET');
+};
+
+export const hentAntallÅpneBehandlingerPerAvklaringsbehov = async () => {
+  const url = `${statistikkApiBaseURL}/behandling-per-avklaringsbehov`;
+  return await fetchProxy<BehandlingPerAvklaringsbehov[]>(url, statistikkApiScope, 'GET');
 };
