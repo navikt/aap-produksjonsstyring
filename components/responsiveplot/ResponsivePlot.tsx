45d3465f4f97c36caebb2b531ddaa63802bcd220
Tor Idland <tor.idland@nav.no>
2024-11-28 09:22:58 +0100

    :lipstick: endret colorway på plots til aksel sin light-palett for datavisualisering

--- components/responsiveplot/ResponsivePlot.tsx
+++ components/responsiveplot/ResponsivePlot.tsx
@@ -8,7 +8,12 @@ export const ResponsivePlot = ({ data, layout }: ResponsivePlotProps) => {
   return (
     <Plot
       data={data}
-      layout={{ ...layout, autosize: true, margin: { t: 40, pad: 10 } }}
+      layout={{
+        ...layout,
+        autosize: true,
+        margin: { t: 40, pad: 10 },
+        colorway: ['#CCE1FF', '#99C4DD', '#FFC166', '#66CBEC', '#99DEAD', '#C0B2D2'],
+      }}
       useResizeHandler={true}
       style={{ width: '100%', height: '100%' }}
     />
