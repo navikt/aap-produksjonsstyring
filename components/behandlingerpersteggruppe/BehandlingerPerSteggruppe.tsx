4c2014baa19134e366a90dfa3622784818ad96ab
Tor Idland <tor.idland@nav.no>
2024-11-29 12:04:21 +0100

    :children_crossing: toggle mellom graf og tabell for stegfordeling

--- components/behandlingerpersteggruppe/BehandlingerPerSteggruppe.tsx
+++ components/behandlingerpersteggruppe/BehandlingerPerSteggruppe.tsx
@@ -3,15 +3,18 @@
 import { BehandlingPerSteggruppe } from 'lib/types/types';
 import { PlotWrapper } from 'components/plotwrapper/PlotWrapper';
 import { ResponsivePlot } from 'components/responsiveplot/ResponsivePlot';
-import { BodyShort, Heading, VStack } from '@navikt/ds-react';
+import { BodyShort, Heading, HStack, Table, ToggleGroup, VStack } from '@navikt/ds-react';
 import { mapTilSteggruppeTekst } from 'lib/utils/oversettelser';
 import { steggruppeRekkefølge } from 'lib/utils/steggruppe';
+import { BarChartIcon, TableIcon } from '@navikt/aksel-icons';
+import { useState } from 'react';
 
 interface Props {
   data: Array<BehandlingPerSteggruppe>;
 }
 
 export const BehandlingerPerSteggruppe = ({ data }: Props) => {
+  const [visning, setVisning] = useState<string>('chart');
   const sorterteSteg: Array<BehandlingPerSteggruppe> = steggruppeRekkefølge.reduce(
     (acc: Array<BehandlingPerSteggruppe>, steggruppe) => {
       const steggruppeIData = data.find((e) => `${e.steggruppe}` === steggruppe);
@@ -23,12 +26,14 @@ export const BehandlingerPerSteggruppe = ({ data }: Props) => {
     },
     []
   );
-  const oversattData = sorterteSteg.map((gruppe) => ({
-    y: [mapTilSteggruppeTekst(gruppe.steggruppe)],
-    x: [gruppe.antall],
-  }));
   return (
     <PlotWrapper>
+      <HStack justify={'end'}>
+        <ToggleGroup defaultValue="chart" size={'small'} onChange={(val) => setVisning(val)}>
+          <ToggleGroup.Item value="chart" icon={<BarChartIcon title="Graf" />} />
+          <ToggleGroup.Item value="table" icon={<TableIcon title="Tabell" />} />
+        </ToggleGroup>
+      </HStack>
       <VStack align={'center'} gap={'5'}>
         <Heading level={'3'} size={'small'}>
           Stegfordeling
@@ -36,18 +41,41 @@ export const BehandlingerPerSteggruppe = ({ data }: Props) => {
         <BodyShort size={'large'}>Hvor ligger behandling i flyten?</BodyShort>
       </VStack>
 
-      <ResponsivePlot
-        data={oversattData.map((xy) => ({
-          ...xy,
-          type: 'bar',
-          orientation: 'h',
-        }))}
-        layout={{
-          xaxis: { title: 'Antall' },
-          showlegend: false,
-          margin: { l: 110 },
-        }}
-      />
+      {visning === 'chart' && (
+        <ResponsivePlot
+          data={sorterteSteg.map((gruppe) => ({
+            y: [mapTilSteggruppeTekst(gruppe.steggruppe)],
+            x: [gruppe.antall],
+            type: 'bar',
+            orientation: 'h',
+          }))}
+          layout={{
+            xaxis: { title: 'Antall' },
+            showlegend: false,
+            margin: { l: 110 },
+          }}
+        />
+      )}
+      {visning === 'table' && (
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Steg</Table.HeaderCell>
+              <Table.HeaderCell>Antall</Table.HeaderCell>
+              <Table.HeaderCell>Eldste oppgave</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {sorterteSteg.toReversed().map((it, i) => (
+              <Table.Row key={`rad-${i}`}>
+                <Table.DataCell>{mapTilSteggruppeTekst(it.steggruppe)}</Table.DataCell>
+                <Table.DataCell>{it.antall}</Table.DataCell>
+                <Table.DataCell>{'finnes ikke'}</Table.DataCell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      )}
     </PlotWrapper>
   );
 };
