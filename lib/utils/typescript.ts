fa3d87afb7626f27a777c6246019fc40aa260c2c
Tor Idland <tor.idland@nav.no>
2025-01-22 11:10:27 +0100

    :label: oppdater typer

--- lib/utils/typescript.ts
+++ lib/utils/typescript.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function exhaustiveCheck(_param: never): never {
-  throw new Error('Skal ikke nå hit');
+  throw new Error(`Skal ikke nå hit: ${_param}`);
 }
