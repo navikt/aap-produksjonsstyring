1b08a2e690e7212ef1308eb134fe34aea252ae36
Tor Idland <tor.idland@nav.no>
2025-02-18 11:23:26 +0100

    :recycle: basepath /oppgave og fjernet ingress og verifyuserloggedin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/oppgave',
   /* config options here */
   output: 'standalone',
   assetPrefix: process.env.ASSET_PREFIX ?? undefined,
